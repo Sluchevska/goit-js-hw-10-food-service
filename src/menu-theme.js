09e868bd22e89cca65b863758bb6e13870dfea8c
Sluchevska <alaska3a@gmail.com>
2021-06-22 23:21:47 +0100

    update

--- src/menu-theme.js
+++ src/menu-theme.js
@@ -1,18 +1,35 @@
 const labelRef = document.querySelector('#theme-switch-toggle')
 const bodyRef = document.querySelector('body')
+const checkboxRef = document.querySelector('#theme-switch-toggle')
 
-
-console.log(labelRef)
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-bodyRef.classList.add('light-theme')
+
+const lightTheme = bodyRef.classList.add('light-theme')
 
 labelRef.addEventListener('change', changeTheme)
 
-function changeTheme(e) {
-bodyRef.classList.toggle('dark-theme')
-    
+function changeTheme() {
    
+   bodyRef.classList.toggle('dark-theme')
+    bodyRef.classList.remove('light-theme')
+     
+   onUpload()
+  
 }
+ 
+function onUpload() {
+    if (!lightTheme) {
+        localStorage.setItem('myTheme', JSON.stringify(Theme.DARK))
+        const savedTheme = localStorage.getItem('myTheme')
+        const parsedTheme = JSON.parse(savedTheme)
+    //   checkboxRef.checked=true
+    } 
+}
+
+
+
+
+
